03ebc982a2f23eb3fd04dce0c0f0543163e4ed16
Iyad <iyadshobaki82@gmail.com>
2025-12-09 15:56:58 -0500

    feat: add weather card and style it

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,12 @@
 import "./WeatherCard.css";
-
+import sunny from "../../assets/images/sunny.png";
 function WeatherCard() {
-  return <></>;
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75&deg;F</p>
+      <img src={sunny} alt="sunny" className="weather-card__image" />
+    </section>
+  );
 }
 
 export default WeatherCard;
