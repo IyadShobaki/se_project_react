864216aef120220fab67b2c688504244335b7f4e
Iyad <iyadshobaki82@gmail.com>
2025-12-10 16:11:58 -0500

    fix: try to fix deploying issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/se_project_react",
-  server: {
-    port: 3000,
-  },
+  base: "/se_project_react",
+  // server: {
+  //   port: 3000,
+  // },
 });
