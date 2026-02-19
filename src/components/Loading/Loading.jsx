7af138b79d04027b5ad0e0c74b70a7cb7e49f00e
iyadshobaki <iyadshobaki82@gmail.com>
2026-02-19 09:26:09 -0500

    style: change the loading animation effect

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,9 +3,9 @@ import "./Loading.css";
 function Loading({ name }) {
   return (
     <div className={name}>
-      <span className="loading__span">.</span>
-      <span className="loading__span">.</span>
-      <span className="loading__span">.</span>
+      <span className="loading__drop" />
+      <span className="loading__drop" />
+      <span className="loading__drop" />
     </div>
   );
 }
