bd12471598787076518fc57c5d54ac9a35534842
iyadshobaki <iyadshobaki82@gmail.com>
2026-02-20 10:45:52 -0500

    style: add style to register and login modals buttons

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -39,39 +39,41 @@ function ModalWithForm({
         ></button>
         <form onSubmit={handleSubmit} className="modal__form" noValidate>
           {children}
-          <button
-            type="submit"
-            disabled={!isValid || isLoading}
-            className={`modal__submit-btn ${
-              !isValid ? "modal__submit-btn_disabled" : ""
-            } ${title === "Change Profile Data" ? "modal__submit-btn_profile" : ""}`}
-          >
-            {isLoading ? getLoadingBtnText(buttonText) : buttonText}
-          </button>
-          {title === "Sign Up" && (
-            <p className="modal__link-text modal__link-text_register">
-              or{" "}
-              <button
-                type="button"
-                className="modal__link-button"
-                onClick={onNavigateLogin}
-              >
-                Log in
-              </button>
-            </p>
-          )}
-          {title === "Log In" && (
-            <p className="modal__link-text modal__link-text_login">
-              or{" "}
-              <button
-                type="button"
-                className="modal__link-button"
-                onClick={onNavigateRegister}
-              >
-                Register
-              </button>
-            </p>
-          )}
+          <div className="modal__button-container">
+            <button
+              type="submit"
+              disabled={!isValid || isLoading}
+              className={`modal__submit-btn ${
+                !isValid ? "modal__submit-btn_disabled" : ""
+              } ${title === "Change Profile Data" ? "modal__submit-btn_profile" : ""}`}
+            >
+              {isLoading ? getLoadingBtnText(buttonText) : buttonText}
+            </button>
+            {title === "Sign Up" && (
+              <p className="modal__link-text">
+                or{" "}
+                <button
+                  type="button"
+                  className="modal__link-button"
+                  onClick={onNavigateLogin}
+                >
+                  Log in
+                </button>
+              </p>
+            )}
+            {title === "Log In" && (
+              <p className="modal__link-text">
+                or{" "}
+                <button
+                  type="button"
+                  className="modal__link-button"
+                  onClick={onNavigateRegister}
+                >
+                  Register
+                </button>
+              </p>
+            )}
+          </div>
         </form>
       </div>
     </div>
